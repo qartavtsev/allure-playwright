992fdc8f72ecbc36bdab87f60839332c94ece888
qartavtsev <alexander.kartavtsev@qameta.io>
2024-03-27 16:43:49 +0300

    first draft

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,3 +1,10 @@
 import { test } from "@playwright/test";
+import { allure } from "allure-playwright";
+page = "www.google.com"
 
-test("sample test", () => {});
+test("Test with attachments", async ({ page }) => {
+  // ...
+  await allure.attachment("search-results.png", await page.screenshot(), {
+    contentType: "image/png",
+  });
+});
